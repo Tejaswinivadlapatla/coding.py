5fc9eded1e983bc7465481c26e28930dc75d8235
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-07-13 14:46:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/String Functions I - Python/string-functions-i-python.py
@@ -0,0 +1,9 @@
+def trim(str):
+    return str.strip()
+def exists(str, x):
+    return str.strip().find(x)
+def titleIt(str):
+    return str.strip().title()
+def casesSwap(str):
+    return str.swapcase()
+
