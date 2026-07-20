6e92e90bf76e3c02b1c0d4b781c2dad2574723b2
VADLAPATLA TEJASWINI <153888789+Tejaswinivadlapatla@users.noreply.github.com>
2026-07-20 11:11:50 +0530

    ✅ Number of Good Pairs | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 42.8 MB | 2026-07-20

---
+++ leetcode/easy/1512-number-of-good-pairs/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for(int num:nums){
+            int freq = map.getOrDefault(num,0);
+            count += freq;
+            map.put(num ,freq+1);
+        }
+        return count;
+    }
+}
